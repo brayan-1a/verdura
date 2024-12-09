3562e878453c25810ffec3b42a530a0caea256e8
brayan-1a <i2414619@continental.edu.pe>
2024-12-09 13:24:08 -0500

    modeloverdura1

---
+++ config.py
@@ -0,0 +1,9 @@
+from supabase import create_client
+
+# URL y API Key de Supabase
+URL = 'https://odlosqyzqrggrhvkdovj.supabase.co'
+KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Ck'
+
+def get_supabase_client():
+    """Crea y devuelve una instancia del cliente Supabase."""
+    return create_client(URL, KEY)
