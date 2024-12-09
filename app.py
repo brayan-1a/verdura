85a6287a35980a3976dd83a7174188ff6c4ded9a
brayan-1a <i2414619@continental.edu.pe>
2024-12-09 13:34:38 -0500

    modeloverdura2

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import streamlit as st
+from config import get_supabase_client
+from preprocess import load_and_select_data
+
+# Conexión con Supabase
+supabase = get_supabase_client()
+
+# Columnas relevantes
 selected_columns = [
     "fecha",
     "producto",
@@ -11,5 +19,10 @@ selected_columns = [
     "ventas_por_hora"
 ]
 
+# Cargar los datos
 df = load_and_select_data(supabase, "verduras", selected_columns)
+
+# Mostrar los datos seleccionados en Streamlit
+st.title("Análisis de Predicción de Stock - Verduras")
 st.write("Datos seleccionados:", df)
+
