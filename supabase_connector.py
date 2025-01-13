31b2c46169a73ad0599007c7cf0957ecf2feb2ec
brayan-1a <i2414619@continental.edu.pe>
2025-01-13 16:45:00 -0500

    modelooo31

--- supabase_connector.py
+++ supabase_connector.py
@@ -1,10 +1,16 @@
 from supabase import create_client, Client
 import os
+from dotenv import load_dotenv 
 
 # Configuración de conexión a Supabase
-url = os.getenv("https://odlosqyzqrggrhvkdovj.supabase.co")  # Asegúrate de configurar estas variables en tu entorno
-key = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Ck")
+url = os.getenv('https://odlosqyzqrggrhvkdovj.supabase.co')  # Asegúrate de configurar estas variables en tu entorno
+key = os.getenv('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Ck')
 
+# Verifica que las variables estén cargadas correctamente
+if not url or not key:
+    raise ValueError("La URL o la clave de Supabase no están configuradas correctamente.")
+
+# Crear el cliente de Supabase
 supabase: Client = create_client(url, key)
 
 def get_data():
