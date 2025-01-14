1bc4957ed3fbfedcaf8022908c0056c7eec5330a
brayan-1a <i2414619@continental.edu.pe>
2025-01-14 01:01:10 -0500

    modelooo56

--- supabase_connector.py
+++ supabase_connector.py
@@ -4,13 +4,25 @@ import pandas as pd
 # Conectar a Supabase
 url = 'https://odlosqyzqrggrhvkdovj.supabase.co'
 key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Ck'
+
 supabase: Client = create_client(url, key)
 
 # Función para cargar productos desde Supabase
 def cargar_productos():
-    productos = supabase.table('productos').select('*').execute()
-    df_productos = pd.DataFrame(productos['data'])
-    return df_productos
+    try:
+        # Intentamos obtener los productos
+        response = supabase.table('productos').select('*').execute()
+        
+        # Verificamos si la clave 'data' está en la respuesta
+        if 'data' in response:
+            df_productos = pd.DataFrame(response['data'])
+            return df_productos
+        else:
+            print("Error: No se encontraron datos en la tabla 'productos'.")
+            return pd.DataFrame()  # Retorna un DataFrame vacío en caso de error
+    except Exception as e:
+        print(f"Error al cargar los productos desde Supabase: {e}")
+        return pd.DataFrame()  # Retorna un DataFrame vacío en caso de error
 
 
 
