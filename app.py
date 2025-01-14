7e36f58685086d7c68b66a8b78b9f401466e6ced
brayan-1a <i2414619@continental.edu.pe>
2025-01-14 00:00:13 -0500

    modelooo50

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ st.title('Predicción de Stock para Verduras')
 
 # Sección de Entrenamiento del modelo
 st.header('Entrenamiento del Modelo')
+
+# Botón para entrenar el modelo
 if st.button('Entrenar Modelo'):
     # Entrenar el modelo y obtener el modelo entrenado y MSE
     modelo, mse = entrenar_modelo()
+    
+    # Guardar el modelo entrenado en la sesión de Streamlit
+    st.session_state.modelo = modelo
+    
     st.success(f'Modelo entrenado exitosamente. Error cuadrático medio (MSE): {mse:.2f}')
 else:
     st.info("Haz clic en el botón para entrenar el modelo.")
@@ -21,8 +27,10 @@ temperatura = st.number_input('Temperatura (°C)', min_value=-50.0, max_value=50
 humedad = st.number_input('Humedad (%)', min_value=0, max_value=100)
 
 if st.button('Predecir Stock'):
-    if 'modelo' in locals():  # Verifica si el modelo ha sido entrenado
-        # Realizar la predicción con el modelo cargado temporalmente
+    # Verifica si el modelo ha sido entrenado y está disponible en la sesión
+    if 'modelo' in st.session_state:
+        modelo = st.session_state.modelo
+        # Realizar la predicción con el modelo cargado desde la sesión
         cantidad_predicha = predecir_stock(modelo, precio_unitario, cantidad_promocion, temperatura, humedad)
         st.write(f'La cantidad recomendada de stock es: {cantidad_predicha}')
     else:
