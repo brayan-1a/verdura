8075899a5ab3bb2f511b0623f072ee4355c152ce
brayan-1a <i2414619@continental.edu.pe>
2025-01-13 22:52:02 -0500

    modelooo42

--- predict_demand.py
+++ predict_demand.py
@@ -1,18 +1,28 @@
-import pickle
+import streamlit as st
+from supabase import create_client
 
-def make_prediction(producto, descuento, temperatura, humedad):
-    # Cargar el modelo entrenado
-    with open('modelo_entrenado.pkl', 'rb') as f:
-        model = pickle.load(f)
+# Conexión a Supabase
+url = "https://odlosqyzqrggrhvkdovj.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Ck"
 
-    # Crear las características para la predicción
-    X_new = [[descuento, temperatura, humedad]]  # Características del producto (según lo que elijas)
-
-    # Realizar la predicción
-    prediccion = model.predict(X_new)[0]
+def main():
+    st.title("Test de conexión Supabase")
     
-    return prediccion
+    try:
+        # Crear cliente
+        supabase = create_client(url, key)
+        
+        # Listar todas las tablas
+        response = supabase.table('ventas').select("*").execute()
+        
+        # Mostrar resultado
+        st.write("Respuesta de Supabase:", response)
+        
+    except Exception as e:
+        st.error(f"Error de conexión: {str(e)}")
 
+if __name__ == "__main__":
+    main()
 
 
 
