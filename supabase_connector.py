58c322162a158a122f60fedd956995be169fad6e
brayan-1a <i2414619@continental.edu.pe>
2025-01-14 01:00:01 -0500

    modelooo55

--- supabase_connector.py
+++ supabase_connector.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Conectar a Supabase
 url = 'https://odlosqyzqrggrhvkdovj.supabase.co'
 key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbG9zcXl6cXJnZ3Jodmtkb3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjgyODksImV4cCI6MjA0NTY0NDI4OX0.z5btFX44Eu30kOBJj7eZKAmOUG62IrTcpXUVhMqK9Ck'
-ssupabase: Client = create_client(url, key)
+supabase: Client = create_client(url, key)
 
 # Función para cargar productos desde Supabase
 def cargar_productos():
